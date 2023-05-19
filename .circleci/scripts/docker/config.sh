02938547b2c034993036b372137ef50b052bede8
bob <bob@laptop.boris>
2023-05-19 20:42:10 +0200

    add docker login n docker push

---
+++ .circleci/scripts/docker/config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat <<EOF >./.bob.docker.config.json
+{
+    "auths": {
+    "https://index.docker.io/v1/": {
+        "auth": "${AUTRE_BOB_DOCKER_AUTH_TOKEN}"
+    }
+    }
+}
+EOF
+
+mdkir -p ~/.docker/
+cat ./.bob.docker.config.json ~/.docker/config.json
